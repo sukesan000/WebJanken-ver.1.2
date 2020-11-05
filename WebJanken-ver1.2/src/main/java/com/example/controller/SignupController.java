4060aec3f4a294c9d533d9b07acac646cbfa5df9
sukesan000 <oyama.k.1997@gmail.com>
2020-11-05 20:42:09 +0900

    ラジオボタンの実装・Get用のコントローラ作成

---
+++ WebJanken-ver1.2/src/main/java/com/example/controller/SignupController.java
@@ -0,0 +1,31 @@
+package com.example.controller;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SignupController {
+	//ラジオボタン実装
+	private Map<String, String> radioGender;
+	//ラジオボタンの初期化メソッド
+	private Map<String,String> initRadioGender(){
+		Map<String, String> radio = new LinkedHashMap<>();
+		//男、女をMapに格納
+		radio.put("男","true");
+		radio.put("女","false");
+		return radio;
+	}
+	
+	@GetMapping("/signup")
+	public String getSignUp(Model model) {
+		//ラジオボタンの初期化メソッド呼び出し
+		radioGender = initRadioGender();
+		//ラジオボタン用のMapをModelに登録
+		model.addAttribute("radioGender", radioGender);
+		return "/html/signup";
+	}
+}
